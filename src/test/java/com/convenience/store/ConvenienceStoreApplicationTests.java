f784ec1b23836cecc3104be84a989e7c8b11dc81
su0907 <202344024@itc.ac.kr>
2026-04-16 20:17:14 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/test/java/com/convenience/store/ConvenienceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.convenience.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvenienceStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
